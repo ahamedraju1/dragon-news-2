753bb54045e0b5616001d1036e2c82e588fc2947
unknown <ahamedraju152@gmail.com>
2025-07-25 19:29:12 +0600

    sticky scroll behaviour implemented

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -22,13 +22,13 @@ const Home = () => {
 
             <main className='w-11/12 mx-auto my-3b grid grid-cols-12 gap-3'>
 
-                <aside className='grid col-span-3'>
+                <aside className='grid col-span-3 sticky top-0 h-fit'>
                     <LeftAside></LeftAside>
                 </aside>
                 <section className="main-content grid col-span-6">
                     <Outlet></Outlet>
                 </section>
-                <aside className='grid col-span-3'>
+                <aside className='grid col-span-3 sticky top-0 h-fit'>
                     <RightAside></RightAside>
                 </aside>
 
